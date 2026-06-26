57c42d36f26639a93ec6092e44b9f98285b24520
Deep-Gajjar-07 <gajjardeep2017@gmail.com>
2026-06-26 16:31:56 +0530

    register models & created base HTML template

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Genre, Movie
+
+admin.site.register(Genre)
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'genre', 'release_year')
+    search_fields = ('title', 'genre')
